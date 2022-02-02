4680f8074fb7ef741d745744b3352a6b58587d93
anushahad <anushahad54@gmail.com>
2022-02-01 22:58:05 -0800

    changes in addprdct scns and prdct model

--- lib/models/ProductModelFile.dart
+++ lib/models/ProductModelFile.dart
@@ -11,6 +11,8 @@ String productModelToJson(ProductModel data) => json.encode(data.toJson());
 class ProductModel {
   ProductModel({
     this.productId,
+    this.brnId,
+    this.cmpId,
     this.productName,
     this.categoryId,
     this.subCategoryId,
@@ -21,11 +23,14 @@ class ProductModel {
     this.barCode,
     this.status,
     this.updatedOn,
+    this.updatedBy,
     this.createdOn,
     this.createdBy,
   });
 
   int ?productId;
+  int ?brnId;
+  int ?cmpId;
   String ?productName;
   int ?categoryId;
   int ?subCategoryId;
@@ -34,13 +39,16 @@ class ProductModel {
   String ?mrp;
   String ?isActive;
   String ?barCode;
-  int ?status;
+  String?status;
   String ?updatedOn;
+  String ?updatedBy;
   String ?createdOn;
   String ?createdBy;
 
   factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
     productId:  null!=json["productId"]?json["productId"] is int ? json["productId"]: int.parse(json["productId"]):-1,
+    brnId:  null!=json["brnId"]?json["brnId"] is int ? json["brnId"]: int.parse(json["brnId"]):-1,
+    cmpId:  null!=json["cmpId"]?json["cmpId"] is int ? json["cmpId"]: int.parse(json["cmpId"]):-1,
     productName: json["productName"],
     categoryId: null!=json["categoryId"]?json["categoryId"] is int ? json["categoryId"]: int.parse(json["categoryId"]):-1,
     subCategoryId: null!=json["subCategoryId"]?json["subCategoryId"] is int ? json["subCategoryId"]: int.parse(json["subCategoryId"]):-1,
@@ -49,14 +57,17 @@ class ProductModel {
     mrp: json["mrp"],
     isActive: json["isActive"],
     barCode: json["barCode"],
-    status:  null!=json["status"]?json["status"] is int ? json["status"]: int.parse(json["status"]):-1,
+    status:  json["status"],
     updatedOn: json["updatedOn"],
+    updatedBy: json["updatedBy"],
     createdOn: json["createdOn"],
     createdBy: json["createdBy"],
   );
 
   Map<String, dynamic> toJson() => {
     "productId": productId,
+    "brnId": brnId,
+    "cmpId": cmpId,
     "productName": productName,
     "categoryId": categoryId,
     "subCategoryId": subCategoryId,
@@ -67,6 +78,7 @@ class ProductModel {
     "barCode": barCode,
     "status": status,
     "updatedOn": updatedOn,
+    "updatedBy": updatedBy,
     "createdOn": createdOn,
     "createdBy": createdBy,
   };
